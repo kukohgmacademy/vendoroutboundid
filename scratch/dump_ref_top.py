17d996b83db192bf8c2c0a4324bb50c3d2eda00e
arby ardiansyah <arbiardiansyah@gmail.com>
2026-09-10 15:50:11 +0700

    update delete scratch

--- scratch/dump_ref_top.py
+++
@@ -1,5 +0,0 @@
-with open('blog/pentingnya-manajemen-risiko-outbound-terbuka.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines[:300]):
-    print(f"{i+1}: {line}", end="")
